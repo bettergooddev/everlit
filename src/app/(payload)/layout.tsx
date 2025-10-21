c53d44aaa7a5706e47e19f33951ba906845cd029
ItsMeSamG <itsmesamg123@gmail.com>
2025-10-21 17:58:24 +0700

    update to payload

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,13 +1,13 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+import config from '@payload-config'
 import '@payloadcms/next/css'
+import type { ServerFunctionClient } from 'payload'
 import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
-import type { ServerFunctionClient } from 'payload'
-import config from '@payload-config'
 
+import { importMap } from './admin/importMap.js'
 import './custom.scss'
-import { importMap } from './admin/importMap'
 
 type Args = {
   children: React.ReactNode
@@ -23,7 +23,7 @@ const serverFunction: ServerFunctionClient = async function (args) {
 }
 
 const Layout = ({ children }: Args) => (
-  <RootLayout importMap={importMap} config={config} serverFunction={serverFunction}>
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
     {children}
   </RootLayout>
 )
