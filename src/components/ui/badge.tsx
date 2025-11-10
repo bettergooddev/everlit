c45d204c852dd8b90edd203d68d69b633448e96b
bettergooddev <dev@bettergood.agency>
2025-11-10 23:43:42 +0700

    badge updated

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,36 +1,19 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from 'react'
 
-import { cn } from "@/utilities/ui"
+export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
-  {
-    variants: {
-      variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground-100",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-)
-
-export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
-
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({ children }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <div
+      className="type-caption font-light capitalize px-3 py-1 rounded-xs"
+      style={{
+        background:
+          'linear-gradient(to bottom right, rgba(17, 88, 102, 0.6), rgba(77, 120, 130, 0.1))',
+      }}
+    >
+      {children}
+    </div>
   )
 }
 
-export { Badge, badgeVariants }
+export { Badge }
