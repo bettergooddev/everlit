e6e5eabec6bbaad4df923ed10ac6f29f850ff7f8
bettergooddev <dev@bettergood.agency>
2025-11-22 20:52:17 +0700

    page transition provider

---
+++ src/providers/PageTransition/index.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React, { createContext, useCallback, use, useState } from 'react'
+
+export interface ContextType {
+  isTransitioning: boolean
+  setTransitioning: (value: boolean) => void
+}
+
+const initialContext: ContextType = {
+  isTransitioning: false,
+  setTransitioning: () => null,
+}
+
+const PageTransitionContext = createContext(initialContext)
+
+export const PageTransitionProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isTransitioning, setTransitioningState] = useState<boolean>(false)
+
+  const setTransitioning = useCallback((value: boolean) => {
+    setTransitioningState(value)
+  }, [])
+
+  return (
+    <PageTransitionContext value={{ isTransitioning, setTransitioning }}>
+      {children}
+    </PageTransitionContext>
+  )
+}
+
+export const usePageTransition = (): ContextType => use(PageTransitionContext)
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { HeaderThemeProvider } from './HeaderTheme'
+import { PageTransitionProvider } from './PageTransition'
 import { PostHogProvider } from './PostHog'
 
 export const Providers: React.FC<{
@@ -8,7 +9,9 @@ export const Providers: React.FC<{
 }> = ({ children }) => {
   return (
     <PostHogProvider>
-      <HeaderThemeProvider>{children}</HeaderThemeProvider>
+      <PageTransitionProvider>
+        <HeaderThemeProvider>{children}</HeaderThemeProvider>
+      </PageTransitionProvider>
     </PostHogProvider>
   )
 }
