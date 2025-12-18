e32bc3e05259b4bd3c39fda442e84954d3dc2396
bettergooddev <dev@bettergood.agency>
2025-12-18 16:58:46 +0700

    dual image and quote glow timing

--- src/blocks/Quote/Component.tsx
+++ src/blocks/Quote/Component.tsx
@@ -6,13 +6,11 @@ import type { Quote as QuoteType } from '@/payload-types'
 import Section from '@/components/Section'
 import { GraphicDoubleQuote } from '@/paths/paths'
 import { GlowDesktop, GlowMobile, GlowTablet } from './glow'
-import { usePageTransition } from '@/providers/PageTransition'
 import { useTextReveal } from '@/hooks/useTextReveal'
 
 export const Quote: React.FC<QuoteType> = ({ quote, backgroundImage }) => {
   const sectionRef = useRef<HTMLDivElement>(null)
-  const isInView = useInView(sectionRef, { once: true, amount: 0.5 })
-  const { isTransitioning } = usePageTransition()
+  const isInView = useInView(sectionRef, { once: true, amount: 0.4 })
   const quoteRef = useTextReveal<HTMLQuoteElement>({
     text: quote,
     start: 'top 75%',
@@ -25,23 +23,16 @@ export const Quote: React.FC<QuoteType> = ({ quote, backgroundImage }) => {
         backgroundImage={backgroundImage}
         className="hidden xl:block"
         inView={isInView}
-        isTransitioning={isTransitioning}
       />
       <GlowTablet
         backgroundImage={backgroundImage}
         className="hidden md:block xl:hidden"
         inView={isInView}
-        isTransitioning={isTransitioning}
       />
-      <GlowMobile
-        backgroundImage={backgroundImage}
-        className="lg:hidden"
-        inView={isInView}
-        isTransitioning={isTransitioning}
-      />
+      <GlowMobile backgroundImage={backgroundImage} className="lg:hidden" inView={isInView} />
       <div className="container text-center relative">
         <GraphicDoubleQuote className="size-16 sm:size-24 lg:size-32 absolute top-1/2 left-0 -translate-y-[calc(50%+8px)] opacity-5 " />
-        <blockquote ref={quoteRef} className="type-h3 max-w-[22ch] mx-auto italic text-pretty">
+        <blockquote ref={quoteRef} className="type-h3 max-w-[42ch] mx-auto italic text-pretty">
           {quote}
         </blockquote>
       </div>
